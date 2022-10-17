32364a4eaa64739ed91ead985fc03a6004b1ab55
julia <yuliya.storozhenko.1994@mail.ru>
2022-10-17 20:44:42 +0400

    FixBugs

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
-        int clientsmoney = 300;
-        int addmoney = 1570;
-        int newmoney;
+        int clientsMoney = 300;
+        int addMoney = 1570;
+        int newMoney;
         int bonus = 0;
-        if (addmoney >= 1000) {
-            bonus = addmoney / 100;
+        if (addMoney >= 1000) {
+            bonus = addMoney / 100;
         }
-        newmoney = clientsmoney + addmoney + bonus;
-        System.out.println(" Счёт клиента " + clientsmoney);
-        System.out.println(" Сумма пополнения " + addmoney);
+        newMoney = clientsMoney + addMoney + bonus;
+        System.out.println(" Счёт клиента " + clientsMoney);
+        System.out.println(" Сумма пополнения " + addMoney);
         System.out.println(" Начислено бонусов " + bonus);
-        System.out.println(" Итоговая сумма " + newmoney);
+        System.out.println(" Итоговая сумма " + newMoney);
 
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
     }
 }
